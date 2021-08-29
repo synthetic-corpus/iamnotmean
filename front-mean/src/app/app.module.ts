90a036a96f38ba6512a722a21fb25dbafc724fdc
Joel G <joel@joelgonzaga.com>
2021-08-29 16:50:14 -0700

    added materials. First experiment.

--- front-mean/src/app/app.module.ts
+++ front-mean/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,10 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
