55f622047a8ce53343da18735708b1c9b4ed8029
Joel G <joel@joelgonzaga.com>
2022-01-02 11:46:48 -0800

    fixed CORS issues.

--- front-mean/backend/app.js
+++ front-mean/backend/app.js
@@ -3,19 +3,16 @@ const express = require('express')
 const app = express()
 
 app.use((req,res,next) => {
-  res.setHeader("Allow-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, Options"
+    "GET, POST, PATCH, DELETE, OPTIONS"
   )
-})
-
-app.use('/test',(req,res,next)=>{
-  res.status(200).json({message:"Api is Up!"})
+  next()
 })
 
 app.use('/api/posts',(req, res, next) => {
